3e0bf26e8d133ea4fbab7e2271880e67c20b6b2c
Volodymyr_Moisei <v.moisei@asd-team.com>
2022-02-18 17:11:21 +0200

    fix createdAt modifiedAt

--- src/main/java/com/asd/mattermostslashcommands/entity/RequestAccessEntity.java
+++ src/main/java/com/asd/mattermostslashcommands/entity/RequestAccessEntity.java
@@ -1,19 +1,17 @@
 package com.asd.mattermostslashcommands.entity;
 
 import com.asd.mattermostslashcommands.enums.RequestAccessState;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +22,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EntityListeners(AuditingEntityListener.class)
 public class RequestAccessEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -34,12 +31,10 @@ public class RequestAccessEntity {
 	private String manager;
 	private String requester;
 	private RequestAccessState state;
-	@CreatedDate
-	@Setter(value = AccessLevel.NONE)
+	@CreationTimestamp
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
-	@LastModifiedDate
-	@Setter(value = AccessLevel.NONE)
+	@UpdateTimestamp
 	@Column(name = "modified_at")
 	private LocalDateTime modifiedAt;
 
